f329e30499ed83d908b3f04c8ad1d5e88a6cb8da
Michal Bradac <m.bradac.michal@gmail.com>
2021-01-23 22:35:43 +0100

    add flags

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,15 @@
-import { AppBar, Toolbar, Typography, makeStyles } from '@material-ui/core'
+import {
+	AppBar,
+	Toolbar,
+	Typography,
+	makeStyles,
+	Select,
+	MenuItem,
+} from '@material-ui/core'
 import React from 'react'
 import { useTranslation } from 'react-i18next'
 import { Link } from 'react-router-dom'
+import Flag from 'react-world-flags'
 
 const useStyles = makeStyles({
 	grow: {
@@ -12,11 +20,14 @@ const useStyles = makeStyles({
 		color: 'inherit',
 		padding: '0 6px',
 	},
+	select: {
+		paddingLeft: '6px',
+	},
 })
 
 const Nav = ({ styles, ...otherProps }: { styles: React.CSSProperties }) => {
 	const classes = useStyles()
-	const [t] = useTranslation()
+	const [t, i18n] = useTranslation()
 
 	return (
 		<AppBar position="fixed" style={styles} {...otherProps}>
@@ -34,6 +45,27 @@ const Nav = ({ styles, ...otherProps }: { styles: React.CSSProperties }) => {
 				<Link to="/prices" className={classes.link}>
 					<Typography variant="h6">{t('prices')}</Typography>
 				</Link>
+				<Select
+					value={i18n.language}
+					onChange={(e: React.ChangeEvent<{ value: unknown }>) =>
+						i18n.changeLanguage(e.target.value as string)
+					}
+					disableUnderline
+					className={classes.select}
+				>
+					<MenuItem value="en">
+						<Flag code="GB" />
+					</MenuItem>
+					<MenuItem value="cs">
+						<Flag code="CZ" />
+					</MenuItem>
+					<MenuItem value="de">
+						<Flag code="DE" />
+					</MenuItem>
+					<MenuItem value="pl">
+						<Flag code="PL" />
+					</MenuItem>
+				</Select>
 			</Toolbar>
 		</AppBar>
 	)
