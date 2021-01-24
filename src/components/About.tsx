35692418c61e5ca6f0a6ea145d3c3dfda4eea579
Michal Bradac <m.bradac.michal@gmail.com>
2021-01-24 11:20:49 +0100

    improve gallery

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -6,13 +6,23 @@ import kitchen from '../images/kitchen.jpg'
 import kitchen2 from '../images/kitchen2.jpg'
 import room from '../images/room.jpg'
 import room2 from '../images/room2.jpg'
-import shower from '../images/shower.jpg'
-import shower2 from '../images/shower2.jpg'
-import sink from '../images/sink.jpg'
 import tables from '../images/tables.jpg'
-import toilet from '../images/toilet.jpg'
+import { makeStyles, Typography } from '@material-ui/core'
+import { useTranslation } from 'react-i18next'
+
+const useStyles = makeStyles({
+	title: {
+		marginBottom: '15px',
+	},
+	galleryWrapper: {
+		maxHeight: '700px',
+	},
+})
 
 const About: React.FunctionComponent = () => {
+	const [t] = useTranslation()
+	const classes = useStyles()
+
 	const images = [
 		{
 			original: beds,
@@ -30,22 +40,24 @@ const About: React.FunctionComponent = () => {
 			original: room2,
 		},
 		{
-			original: shower,
-		},
-		{
-			original: shower2,
-		},
-		{
-			original: sink,
-		},
-		{
 			original: tables,
 		},
-		{
-			original: toilet,
-		},
 	]
-	return <ImageGallery items={images} showThumbnails={false} showBullets />
+	return (
+		<>
+			<Typography variant="h2" className={classes.title}>
+				{t('gallery')}
+			</Typography>
+			<div className={classes.galleryWrapper}>
+				<ImageGallery
+					items={images}
+					showThumbnails={false}
+					showBullets
+					slideDuration={300}
+				/>
+			</div>
+		</>
+	)
 }
 
 export default About
