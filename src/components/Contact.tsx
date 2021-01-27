d855554b64ea67a68122876278a8939b58e8257f
Michal Bradáč <m.bradac.michal@gmail.com>
2021-01-27 09:57:34 +0100

    add emoji to the contact and home

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -3,66 +3,66 @@ import React from 'react'
 import { useTranslation } from 'react-i18next'
 
 const useStyles = makeStyles({
-	card: {
-		padding: '14px',
-		margin: '5px',
-	},
-	contactTitle: {
-		fontSize: '1.5rem',
-	},
+  card: {
+    padding: '14px',
+    margin: '5px',
+  },
+  contactTitle: {
+    fontSize: '1.5rem',
+  },
 })
 
 const Contact: React.FunctionComponent = () => {
-	const [t] = useTranslation()
-	const classes = useStyles()
+  const [t] = useTranslation()
+  const classes = useStyles()
 
-	return (
-		<>
-			<Card variant="outlined" className={classes.card}>
-				<Typography
-					variant="h3"
-					color="textSecondary"
-					className={classes.contactTitle}
-				>
-					{t('reservationContact')}
-				</Typography>
-				<Typography>
-					{t('telephone')}:{' '}
-					<span itemProp="telephone">
-						<a href="tel:+420 272 760 329">+420 272 760 329</a>
-					</span>
-					<br />
-					{t('email')}:{' '}
-					<span itemProp="email">
-						<a href="mailto:alter@alter.cz">alter@alter.cz</a>
-					</span>
-				</Typography>
-			</Card>
-			<Card variant="outlined" className={classes.card}>
-				<Typography
-					variant="h3"
-					color="textSecondary"
-					className={classes.contactTitle}
-				>
-					{t('address')}
-				</Typography>
-				<Typography>
-					<div
-						itemProp="address"
-						itemScope
-						itemType="https://schema.org/PostalAddress"
-					>
-						{t('street')}:{' '}
-						<span itemProp="streetAddress">Horní Rokytnice 26</span>
-						<br />
-						{t('locality')}:{' '}
-						<span itemProp="addressLocality">Rokytnice nad Jizerou</span>,{' '}
-						<span itemProp="postalCode">512 44</span>
-					</div>
-				</Typography>
-			</Card>
-		</>
-	)
+  return (
+    <>
+      <Card variant="outlined" className={classes.card}>
+        <Typography
+          variant="h3"
+          color="textSecondary"
+          className={classes.contactTitle}
+        >
+          {t('reservationContact')}
+        </Typography>
+        <Typography>
+          📞 {t('telephone')}:{' '}
+          <span itemProp="telephone">
+            <a href="tel:+420 272 760 329">+420 272 760 329</a>
+          </span>
+          <br />
+          📬 {t('email')}:{' '}
+          <span itemProp="email">
+            <a href="mailto:alter@alter.cz">alter@alter.cz</a>
+          </span>
+        </Typography>
+      </Card>
+      <Card variant="outlined" className={classes.card}>
+        <Typography
+          variant="h3"
+          color="textSecondary"
+          className={classes.contactTitle}
+        >
+          🏡 {t('address')}
+        </Typography>
+        <Typography>
+          <div
+            itemProp="address"
+            itemScope
+            itemType="https://schema.org/PostalAddress"
+          >
+            {t('street')}:{' '}
+            <span itemProp="streetAddress">Horní Rokytnice 26</span>
+            <br />
+            {t('locality')}:{' '}
+            <span itemProp="addressLocality">Rokytnice nad Jizerou</span>,{' '}
+            <span itemProp="postalCode">512 44</span>
+          </div>
+        </Typography>
+      </Card>
+    </>
+  )
 }
 
 export default Contact
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -5,42 +5,44 @@ import { useTranslation } from 'react-i18next'
 import montanaImg from '../images/montana.jpg'
 
 const useStyles = makeStyles({
-	img: {
-		margin: '0 auto',
-		display: 'block',
-		maxWidth: '90vw',
-	},
-	title: {
-		fontSize: '1.5rem',
-	},
-	paper: {
-		marginTop: '25px',
-		padding: '14px',
-	},
+  img: {
+    margin: '0 auto',
+    display: 'block',
+    maxWidth: '90vw',
+  },
+  title: {
+    fontSize: '1.5rem',
+  },
+  paper: {
+    marginTop: '25px',
+    padding: '14px',
+  },
 })
 
 const Home: React.FunctionComponent = () => {
-	const classes = useStyles()
-	const [t] = useTranslation()
+  const classes = useStyles()
+  const [t] = useTranslation()
 
-	return (
-		<>
-			<img
-				itemProp="image"
-				src={montanaImg}
-				alt="Chata"
-				className={classes.img}
-			/>
-			<Paper elevation={2} className={classes.paper}>
-				<Typography variant="h3" className={classes.title}>
-					{t('homeTitle')}
-				</Typography>
-				<Typography>
-					<p>{t('homeDescription')}</p>
-				</Typography>
-			</Paper>
-		</>
-	)
+  return (
+    <>
+      <img
+        itemProp="image"
+        src={montanaImg}
+        alt="Chata"
+        className={classes.img}
+      />
+      <Paper elevation={2} className={classes.paper}>
+        <Typography variant="h3" className={classes.title}>
+          {t('homeTitle')}
+        </Typography>
+        <Typography>
+          <p>{t('homeDescription')}</p>
+          <p>🚭 {t('noSmoking')}</p>
+          <p>🐶 {t('dogFriendly')}</p>
+        </Typography>
+      </Paper>
+    </>
+  )
 }
 
 export default Home
